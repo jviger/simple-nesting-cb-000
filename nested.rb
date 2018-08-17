3fbef8e9abe1105a6126dcefb35443e6730f9563
Jason Viger <jason.viger@gmail.com>
2018-08-17 05:11:25 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => ["LISP", "C"]}
+programmer_hash >> :yuikiro_matsumoto
 
 return programmer_hash[:yukihiro_matsumoto]
     
